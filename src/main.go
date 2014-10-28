b3119a0ded0b67c14948090f3cc4e61f87e1c52a
徐晨阳 <xuender@gmail.com>
2014-10-28 20:26:14 +0800

    取消martini-session

--- src/main.go
+++ src/main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/gzip"
 	"github.com/martini-contrib/render"
-	"github.com/martini-contrib/sessions"
 	//"gopkg.in/mgo.v2"
 	//"gopkg.in/mgo.v2/bson"
 	"./web"
@@ -27,25 +26,10 @@ func main() {
 
 	log.Info("启动服务")
 	m := martini.Classic()
-	// sessions
-	store := sessions.NewCookieStore([]byte("secret123"))
-	m.Use(sessions.Sessions("dou_session", store))
 	// gzip支持
 	m.Use(gzip.All())
 	// 模板支持
 	m.Use(render.Renderer())
-	// 测试session保存
-	m.Get("/set", func(session sessions.Session) string {
-		session.Set("id", "world")
-		return "OK"
-	})
-	m.Get("/get", func(session sessions.Session) string {
-		v := session.Get("id")
-		if v == nil {
-			return ""
-		}
-		return v.(string)
-	})
 	//m.Get("/", func(r render.Render) {
 	//  r.HTML(200, "index", {"title":"title index"})
 	//})
