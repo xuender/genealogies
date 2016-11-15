e030fe9aff9758ae776179e810600310d301b0c6
徐晨阳 <xuender@gmail.com>
2016-11-15 17:28:21 +0800

    初始化 update #8

--- server/main.go
+++ server/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/satori/go.uuid"
+)
+
 func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		m := []string{}
+		for i := 0; i < 10; i++ {
+			m = append(m, uuid.NewV4().String())
+		}
+		c.JSON(200, gin.H{
+			"message": m,
+		})
+	})
+	r.Run() // listen and server on 0.0.0.0:8080
 }
