939850f60e2c7ce11b4930b454e55f816d2b903e
徐晨阳 <xuender@gmail.com>
2016-12-04 17:30:04 +0800

    删除调试代码

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,8 +30,6 @@ export class MyApp {
     this.platform.setLang('zh', true);
     // console.debug(this.platform.lang());
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
