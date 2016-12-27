26e6c4ae2e2d8c0369b6c6d08648f9b916db2b6c
徐晨阳 <xuender@gmail.com>
2016-12-27 19:29:42 +0800

    fix #73

--- src/tree/unknown.ts
+++ src/tree/unknown.ts
@@ -6,17 +6,21 @@ import { filter, count } from '../utils/array';
  * 问题列表
  */
 export class Unknown {
-  id: number;
-  unknown: string[];
+  private static START = 10;
+  private static NO_NAME = 9;
+  private static NO_OTHER = 8;
+  private static NO_CONSORT = 7;
+  private static ERROR_DOB = 6;
+  private static ERROR_DOD = 5;
+  private static NO_DOB = 4;
+  private static NO_DOD = 3;
 
   public static findUnknowns(tree: Tree): Unknown[] {
     const us: Unknown[] = [];
     tree.unknown = 0;
-    let id = 1;
     nodeEach(tree.root, (n: TreeNode) => {
       const u = new Unknown(n);
-      u.id = id;
-      id += 1;
+      u.checkStart();
       u.checkName();
       u.checkDob();
       u.checkDod();
@@ -29,18 +33,33 @@ export class Unknown {
         us.push(u);
       }
     });
-    us.sort((a: Unknown, b: Unknown) => a.node.star === b.node.star ? a.id - b.id : a.node.star ? -1 : 1);
+    us.sort((a: Unknown, b: Unknown) => b.num - a.num);
     return us;
   }
 
+  private num: number;
+  unknown: string[];
+
   constructor(private node: TreeNode) {
     this.unknown = [];
+    this.num = 0;
+  }
+
+  checkStart() {
+    if (this.node.star) {
+      this.portion(Unknown.START);
+    }
+  }
+
+  portion(num: number) {
+    this.num += 1 << num;
   }
 
   checkName() {
     for (const s of ['无名', '妻子', '丈夫', '父亲', '奶奶', '祖母', '儿子', '妈妈', '女儿', '姐姐', '哥哥', '爷爷', '祖父']) {
       if (this.node.name.indexOf(s) >= 0) {
         this.unknown.push('姓名未知');
+        this.portion(Unknown.NO_NAME);
         return;
       }
     }
@@ -56,10 +75,12 @@ export class Unknown {
         }
         if (olds.length > 0) {
           this.unknown.push(`年龄小于: ${olds.join(', ')}`);
+          this.portion(Unknown.ERROR_DOB);
         }
       }
     } else {
       this.unknown.push('出生日期未知');
+      this.portion(Unknown.NO_DOB);
     }
   }
 
@@ -71,6 +92,7 @@ export class Unknown {
           const dob = new Date(this.node.dob);
           if (dob > dod) {
             this.unknown.push('忌日小于生日');
+            this.portion(Unknown.ERROR_DOD);
           }
         }
         if (this.node.children) {
@@ -80,10 +102,12 @@ export class Unknown {
           }
           if (olds.length > 0) {
             this.unknown.push(`忌日早于子女生日: ${olds.join(', ')}`);
+            this.portion(Unknown.ERROR_DOD);
           }
         }
       } else {
         this.unknown.push('忌日未知');
+        this.portion(Unknown.NO_DOD);
       }
     }
   }
@@ -94,6 +118,7 @@ export class Unknown {
       const on = count(this.node.children, (c: TreeNode) => c.nt > NodeType.DEFAULT);
       if (cn > 0 && on === 0) {
         this.unknown.push('有子女无伴侣');
+        this.portion(Unknown.NO_CONSORT);
       }
     }
   }
@@ -112,6 +137,7 @@ export class Unknown {
         const on = count(p.children, (c: TreeNode) => c.nt > NodeType.DEFAULT);
         if (on > 1) {
           this.unknown.push(`${p.gender ? '母亲' : '父亲'}未知`);
+          this.portion(Unknown.NO_OTHER);
         }
       }
     }
