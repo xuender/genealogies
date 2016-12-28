362a05bbf573e2b7f16eb4e44dec4b5eeb090f40
徐晨阳 <xuender@gmail.com>
2016-12-28 12:56:07 +0800

    fix #89

--- karma.conf.js
+++ karma.conf.js
@@ -2,45 +2,45 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', 'angular-cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      // require('karma-phantomjs-launcher'),
-      require('karma-remap-istanbul'),
-      require('karma-mocha-reporter'),
-      require('angular-cli/plugins/karma')
-    ],
-    files: [
-      { pattern: './src/test.ts', watched: false }
-    ],
-    preprocessors: {
-      './src/test.ts': ['angular-cli']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
-    remapIstanbulReporter: {
-      reports: {
-        html: 'coverage',
-        lcovonly: './coverage/coverage.lcov'
-      }
-    },
-    angularCli: {
-      config: './angular-cli.json',
-      environment: 'dev'
-    },
-    reporters: [
-      'mocha', 'karma-remap-istanbul'
-    ],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    // browsers: ['PhantomJS'],
-    singleRun: false
-  });
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', 'angular-cli'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-phantomjs-launcher'),
+            require('karma-remap-istanbul'),
+            require('karma-mocha-reporter'),
+            require('karma-coverage'),
+            require('karma-remap-coverage'),
+            require('angular-cli/plugins/karma')
+        ],
+        files: [
+            { pattern: './src/test.ts', watched: false }
+        ],
+        preprocessors: {
+            './src/test.ts': ['angular-cli']
+        },
+        mime: {
+            'text/x-typescript': ['ts','tsx']
+        },
+        remapIstanbulReporter: {
+            reports: {
+                html: 'coverage',
+                lcovonly: './coverage/coverage.lcov'
+            }
+        },
+        angularCli: {
+            config: './angular-cli.json',
+            environment: 'dev'
+        },
+        reporters: ['mocha', 'coverage', 'remap-coverage'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        // browsers: ['PhantomJS'],
+        singleRun: false
+    });
 };
